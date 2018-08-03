6acbc9248bf3fab706e8eeacb22f5796d80bd85f
inari111 <takuyakoori+github@gmail.com>
2018-08-03 11:49:32 +0900

    buildしたバイナリを使ってswagerを生成するようにする

--- example/generate-swagger.sh
+++ example/generate-swagger.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-go get github.com/elliots/protoc-gen-twirp_swagger
+DIR=$(pwd)
+cd ..
+go build
+cd ${DIR}
 
 protoc --go_out=. \
+       --plugin=../protoc-gen-twirp_swagger \
        --twirp_out=. \
        --twirp_swagger_out=. \
        ./service.proto
